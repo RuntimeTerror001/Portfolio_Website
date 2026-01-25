6d583ff661f09d7aa66a36306262a044574b66f9
Aahaan <aahaansethi1001@gmail.com>
2026-01-25 22:58:45 +0530

    undo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import React from "react";
+=import React from "react";
 import Navbar from "./components/navbar/navbar.jsx";
 import Hero from "./components/hero/Hero.jsx";
 import Aboutme from "./components/aboutme/Aboutme.jsx";
 import Projects from "./components/projects/projects.jsx";
 import Contact from "./components/contact/contact.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import { Routes, Route } from "react-router-dom";
-import './index.css';
+import "./index.css";
+
 function App() {
   return (
     <>
       <Navbar />
 
       <main className="page-content">
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<Aboutme />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Hero />
+        <Aboutme />
+        <Projects />
+        <Contact />
       </main>
 
       <Footer />
@@ -26,6 +24,4 @@ function App() {
   );
 }
 
-
 export default App;
-
